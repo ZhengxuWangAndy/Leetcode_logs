69282e22b103a98b2bf1f6d1d6184c685c62b7be
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-10-15 18:54:00 -0400

    Oct 15

---
+++ 608.sql
@@ -0,0 +1,32 @@
+-- Tree Node
+
+-- Each node in the tree can be one of three types:
+
+-- "Leaf": if the node is a leaf node.
+-- "Root": if the node is the root of the tree.
+-- "Inner": If the node is neither a leaf node nor a root node.
+-- Write a solution to report the type of each node in the tree.
+
+-- Return the result table in any order.
+
+
+-- separatly select these 3 types and union them all
+select id, 'Inner' as type
+from Tree
+where id in (
+    select distinct p_id
+    from Tree
+    where p_id is not null
+) and p_id is not null
+union all
+select id, 'Root' as type
+from Tree
+where p_id is null
+union all
+select id, 'Leaf' as type
+from Tree
+where id not in (
+    select distinct p_id
+    from Tree
+    where p_id is not null
+) and p_id is not null
