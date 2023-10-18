179c3fed3b10b22af6764ba21848164f7efa5b54
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-10-17 23:15:34 -0400

    Oct 17

--- 11.java
+++ 11.java
@@ -25,3 +25,23 @@ class Solution {
         return result;
     }
 }
+
+
+// Official solution:
+public class Solution {
+    public int maxArea(int[] height) {
+        int l = 0, r = height.length - 1;
+        int ans = 0;
+        while (l < r) {
+            int area = Math.min(height[l], height[r]) * (r - l);
+            ans = Math.max(ans, area);
+            if (height[l] <= height[r]) {
+                ++l;
+            }
+            else {
+                --r;
+            }
+        }
+        return ans;
+    }
+}
