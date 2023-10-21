f4617fbfc8cb93c951f7e87bc0585ea6c9338c29
Zhengxu Wang <zhengxuwang519@gmail.com>
2023-10-20 22:50:02 -0400

    Oct 20

---
+++ 104.java
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+// my code: dfs traverse
+class Solution {
+    int depth = 0;
+    public int maxDepth(TreeNode root) {
+        depth(root,1);
+        return depth;
+    }
+
+    public void depth(TreeNode root, int cur){
+        if(root == null){
+            return;
+        }
+        depth = Math.max(cur, depth);
+        depth(root.left, cur+1);
+        depth(root.right, cur+1);
+    }
+}
+
+
+
+// official code: BFS and expand by level
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            while (size > 0) {
+                TreeNode node = queue.poll();
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+                size--;
+            }
+            ans++;
+        }
+        return ans;
+    }
+}
